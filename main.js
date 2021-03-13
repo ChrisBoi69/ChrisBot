5f258de60c033127fb1a3b4e672d194484ccf616
ChrisBoi69 <Krisjanis.dirba@gmail.com>
2021-03-13 15:05:49 +0200

    rtrtrtrtrt

--- main.js
+++ main.js
@@ -103,8 +103,7 @@ if (command === 'wiki') {
     client.commands.get('wiki').execute(message, args, client);
 }
 if (command === 'ping') {
-    client.commands.get('ping').execute(client, message, args);
-    message.react('✅');
+    client.commands.get('ping').execute(client, message, arg);
 }
 
 
